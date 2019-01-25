79e66534ae1b001ef9d5334cee249fd4157aa321
msunkist99 <msunkist99@gmail.com>
2019-01-24 19:33:12 -0600

    20190124 - add if code

--- Month/Program.cs
+++ Month/Program.cs
@@ -9,10 +9,64 @@ namespace Month
             Console.Write("Enter the number of the month: ");
 
             // Save the input to a variable.
+            string monthNumberString = Console.ReadLine();
+            int monthNumber = Convert.ToInt32(monthNumberString);
 
             // Use the if statment to update the monthName variable based on the user's input.
             string monthName = string.Empty;
 
+            if (monthNumber == 1)
+            {
+                monthName = "January";
+            }
+            else if (monthNumber == 2)
+            {
+                monthName = "February";
+            }
+            else if (monthNumber == 3)
+            {
+                monthName = "March";
+            }
+            else if (monthNumber == 4)
+            {
+                monthName = "April";
+            }
+            else if (monthNumber == 5)
+            {
+                monthName = "May";
+            }
+            else if (monthNumber == 6)
+            {
+                monthName = "June";
+            }
+            else if (monthNumber == 7)
+            {
+                monthName = "July";
+            }
+            else if (monthNumber == 8)
+            {
+                monthName = "August";
+            }
+            else if (monthNumber == 9)
+            {
+                monthName = "September";
+            }
+            else if (monthNumber == 10)
+            {
+                monthName = "October";
+            }
+            else if (monthNumber == 11)
+            {
+                monthName = "November";
+            }
+            else if (monthNumber == 12)
+            {
+                monthName = "December";
+            }
+            else
+            {
+                monthName = "Unknown";
+            }
 
             Console.WriteLine($"The name of the month you entered is {monthName}");
 
